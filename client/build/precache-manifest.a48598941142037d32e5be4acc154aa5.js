0ea7dd20cc74c76803d3d90878e35c5c4e56c3cd
latha n <latha9510@gmail.com>
2020-06-08 16:16:07 +0530

    builld

---
+++ client/build/precache-manifest.a48598941142037d32e5be4acc154aa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea1735a4ef6d7d2ffdf062bbe16569d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1188336d4a78de728b6b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1188336d4a78de728b6b",
+    "url": "/static/js/2.51bb00ed.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.51bb00ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d30cabc72f6ae9dcec",
+    "url": "/static/js/main.016465fb.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-const URL = window.location.origin.includes('localhost') ? "http://localhost:3099/api" : "/"
+const URL = window.location.origin.includes('localhost') ? "http://localhost:3099" : "/"
 
 const axios=Axios.create({
     baseURL: URL
